99d1c5444d7268a9265b8a4c0d1f61d6ddc6ff88
AtmoOmen <atmoomen2004@outlook.com>
2024-12-21 21:16:11 +0800

    自动速行: 格式一致调整

--- Action/AutoPeloton.cs
+++ Action/AutoPeloton.cs
@@ -1,86 +1,65 @@
 using System.Collections.Generic;
-
+using DailyRoutines.Abstracts;
+using DailyRoutines.Managers;
 using Dalamud.Game.ClientState.Conditions;
-using Dalamud.Plugin.Services;
-
 using FFXIVClientStructs.FFXIV.Client.Game;
 using FFXIVClientStructs.FFXIV.Client.Game.Control;
-using FFXIVClientStructs.FFXIV.Client.System.Framework;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
-
-using Lumina.Excel.GeneratedSheets;
-using OmenTools;
-using OmenTools.Helpers;
-using OmenTools.Infos;
-
-using DailyRoutines.Managers;
-using DailyRoutines.Abstracts;
-using ImGuiNET;
-using static DailyRoutines.AutoPeloton.AutoPeloton;
-using DailyRoutines.Helpers;
-using System.Linq;
-using Lumina.Excel.GeneratedSheets2;
 
 namespace DailyRoutines.AutoPeloton;
 
 public class AutoPeloton : DailyModuleBase
 {
-    private readonly uint s_PelotoningActionId = 7557;
-
-    // 诗人 机工 舞者
-    private readonly HashSet<uint> s_ClassJobArr = [23, 31, 38];
-
     public override ModuleInfo Info => new()
     {
-        Title = GetLoc("AutoPelotonTitle"), // "自动释放速行"
+        Title       = GetLoc("AutoPelotonTitle"),       // "自动释放速行"
         Description = GetLoc("AutoPelotonDescription"), // "使用远敏职业时，自动尝试释放速行"
-        Category = ModuleCategories.Action,
-        Author = ["N/A"],
+        Category    = ModuleCategories.Action,
+        Author      = ["N/A"]
     };
+    
+    // 诗人 机工 舞者
+    private readonly HashSet<uint> s_ClassJobArr = [23, 31, 38];
+    
+    private readonly uint s_PelotoningActionId = 7557;
 
-    public class Configs : ModuleConfiguration
+    private Configs Config = null!;
+
+    public override void Init()
     {
-        public bool OnlyInDuty = true;
-        public bool DisableInWalk = true;
-    };
+        TaskHelper ??= new TaskHelper { TimeLimitMS = 30_000 };
+        Config     =   LoadConfig<Configs>() ?? new();
 
-    public Configs Config { get; private set; }
+        DService.ClientState.TerritoryChanged += OnTerritoryChanged;
+        DService.DutyState.DutyRecommenced    += OnDutyRecommenced;
+        DService.Condition.ConditionChange    += OnConditionChanged;
+        DService.ClientState.LevelChanged     += OnLevelChanged;
+        DService.ClientState.ClassJobChanged  += OnClassJobChanged;
+
+        TaskHelper.Enqueue(OneTimeConditionCheck);
+    }
+    
     public override void ConfigUI()
     {
         if (ImGui.Checkbox(GetLoc("AutoPeloton-OnlyInDuty"), ref Config.OnlyInDuty)) // "只在副本中使用"
         {
             SaveConfig(Config);
+            
             TaskHelper.Abort();
             TaskHelper.Enqueue(OneTimeConditionCheck);
         }
+
         if (ImGui.Checkbox(GetLoc("AutoPeloton-DisableInWalk"), ref Config.DisableInWalk)) // "走路模式时禁用"
-        {
             SaveConfig(Config);
-        }
-    }
-
-    public override void Init()
-    {
-        TaskHelper ??= new TaskHelper { TimeLimitMS = 30_000 };
-        Config = LoadConfig<Configs>() ?? new Configs();
-
-        DService.ClientState.TerritoryChanged += OnTerritoryChanged;
-        DService.DutyState.DutyRecommenced += OnDutyRecommenced;
-        DService.Condition.ConditionChange += OnConditionChanged;
-        DService.ClientState.LevelChanged += OnLevelChanged;
-        DService.ClientState.ClassJobChanged += OnClassJobChanged;
-
-        if (!TaskHelper.IsBusy) TaskHelper.Enqueue(OneTimeConditionCheck);
-
     }
 
     public override void Uninit()
     {
         DService.ClientState.TerritoryChanged -= OnTerritoryChanged;
-        DService.DutyState.DutyRecommenced -= OnDutyRecommenced;
-        DService.Condition.ConditionChange -= OnConditionChanged;
-        DService.ClientState.LevelChanged -= OnLevelChanged;
-        DService.ClientState.ClassJobChanged -= OnClassJobChanged;
+        DService.DutyState.DutyRecommenced    -= OnDutyRecommenced;
+        DService.Condition.ConditionChange    -= OnConditionChanged;
+        DService.ClientState.LevelChanged     -= OnLevelChanged;
+        DService.ClientState.ClassJobChanged  -= OnClassJobChanged;
 
         if (Config != null) SaveConfig(Config);
 
@@ -99,8 +78,7 @@ public class AutoPeloton : DailyModuleBase
     {
         TaskHelper.Abort();
 
-        if (Config.OnlyInDuty && (GameMain.Instance()->CurrentContentFinderConditionId == 0)) return;
-
+        if (Config.OnlyInDuty && GameMain.Instance()->CurrentContentFinderConditionId == 0) return;
         TaskHelper.Enqueue(OneTimeConditionCheck);
     }
 
@@ -108,9 +86,6 @@ public class AutoPeloton : DailyModuleBase
     private void OnLevelChanged(uint classJobId, uint level)
     {
         TaskHelper.Abort();
-
-        if (!HelpersOm.IsActionUnlocked(s_PelotoningActionId)) return;
-
         TaskHelper.Enqueue(OneTimeConditionCheck);
     }
 
@@ -135,7 +110,7 @@ public class AutoPeloton : DailyModuleBase
     private unsafe bool? OneTimeConditionCheck()
     {
         // re-entry by OnTerritoryChanged()
-        if (Config.OnlyInDuty && (GameMain.Instance()->CurrentContentFinderConditionId == 0)) return true;
+        if (Config.OnlyInDuty && GameMain.Instance()->CurrentContentFinderConditionId == 0) return true;
         if (GameMain.IsInPvPArea() || GameMain.IsInPvPInstance()) return true;
         // re-entry by OnConditionChanged()
         if (DService.Condition[ConditionFlag.InCombat]) return true;
@@ -146,49 +121,45 @@ public class AutoPeloton : DailyModuleBase
 
     private bool Cycle(int delayMs = 0)
     {
-        if (delayMs > 0)
-        {
-            TaskHelper.DelayNext(delayMs);
-        }
+        if (delayMs > 0) TaskHelper.DelayNext(delayMs);
         TaskHelper.Enqueue(MainProcess);
         return true;
     }
 
     private unsafe bool? MainProcess()
     {
-        if (InfosOm.BetweenAreas || !HelpersOm.IsScreenReady() || InfosOm.OccupiedInEvent) return Cycle(1_000);
+        if (BetweenAreas || !IsScreenReady() || OccupiedInEvent) return Cycle(1_000);
         if (DService.ClientState.LocalPlayer is not { } localPlayer) return Cycle(1_000);
         if (!s_ClassJobArr.Contains(localPlayer.ClassJob.Id)) return true;
-        if (!HelpersOm.IsActionUnlocked(s_PelotoningActionId)) return true;
+        if (!IsActionUnlocked(s_PelotoningActionId)) return true;
 
-        if (Config.DisableInWalk && (Control.Instance()->IsWalking)) return Cycle(1_000);
+        if (Config.DisableInWalk && Control.Instance()->IsWalking) return Cycle(1_000);
 
         TaskHelper.Enqueue(UsePeloton, "UsePeloton", 5_000, true, 1);
-
         return Cycle(1_000);
     }
 
     private unsafe bool? UsePeloton()
     {
         if (DService.ClientState.LocalPlayer is not { } localPlayer) return false;
-        ActionManager* am = ActionManager.Instance();
+        var actionManager = ActionManager.Instance();
         var statusManager = localPlayer.ToBCStruct()->StatusManager;
 
         // PeletonNotReady
-        if (am->GetActionStatus(ActionType.Action, s_PelotoningActionId) != 0) return true;
+        if (actionManager->GetActionStatus(ActionType.Action, s_PelotoningActionId) != 0) return true;
         // AlreadyHasPeletonBuff
         if (statusManager.HasStatus(1199) || statusManager.HasStatus(50)) return true;
         // NotMoving
         if (AgentMap.Instance()->IsPlayerMoving != 1) return true;
 
-        TaskHelper.Enqueue(() => UseActionManager.UseAction(
-            ActionType.Action,
-            s_PelotoningActionId),
-            $"UseAction_{s_PelotoningActionId}",
-            5_000,
-            true,
-            1
-        );
+        TaskHelper.Enqueue(() => UseActionManager.UseAction(ActionType.Action, s_PelotoningActionId),
+                           $"UseAction_{s_PelotoningActionId}", 5_000, true, 1);
         return true;
+    }
+    
+    private class Configs : ModuleConfiguration
+    {
+        public bool OnlyInDuty    = true;
+        public bool DisableInWalk = true;
     }
 }
