5fb876b8a7f8681bb57d86755b199678bc746624
Vanillaaaa <vanillaaaajp@gmail.com>
2025-02-28 15:46:05 +0800

    Update ReuseEmote.cs

--- General/ReuseEmote.cs
+++ General/ReuseEmote.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using DailyRoutines.Abstracts;
-using DailyRoutines.Infos;
 using DailyRoutines.Managers;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 
