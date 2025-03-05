50238fbc69eb265daa67d3db6ef31f04a60a0aad
AtmoOmen <atmoomen2004@outlook.com>
2025-03-05 22:10:55 +0800

    ClipboardMultiLineToOneLine

--- System/ClipboardMultiLineToOneLine.cs
+++ System/ClipboardMultiLineToOneLine.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using DailyRoutines.Abstracts;
 using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
@@ -5,6 +7,7 @@ using Dalamud.Hooking;
 using FFXIVClientStructs.FFXIV.Client.System.Framework;
 using FFXIVClientStructs.FFXIV.Client.System.String;
 using System.Windows.Forms;
+using FFXIVClientStructs.FFXIV.Client.UI;
 
 namespace DailyRoutines.Modules;
 
@@ -12,9 +15,9 @@ public unsafe class ClipboardMultiLineToOneLine : DailyModuleBase
 {
     public override ModuleInfo Info => new()
     {
-        Title = GetLoc("ClipboardMultiLineToOneLineTitle"),
+        Title       = GetLoc("ClipboardMultiLineToOneLineTitle"),
         Description = GetLoc("ClipboardMultiLineToOneLineDescription"),
-        Category = ModuleCategories.System,
+        Category    = ModuleCategories.System,
     };
 
     private static readonly CompSig GetClipboardDataSig =
@@ -23,51 +26,44 @@ public unsafe class ClipboardMultiLineToOneLine : DailyModuleBase
     private static Hook<GetClipboardDataDelegate>? GetClipboardDataHook;
 
     private static readonly string[] BlacklistAddons = ["Macro"];
-    private static bool IsBlocked;
 
     public override void Init()
     {
-        
-        GetClipboardDataHook ??= DService.Hook.HookFromSignature<GetClipboardDataDelegate>(
-            GetClipboardDataSig.Get(), GetClipboardDataDetour);
+        GetClipboardDataHook ??= GetClipboardDataSig.GetHook<GetClipboardDataDelegate>(GetClipboardDataDetour);
         GetClipboardDataHook.Enable();
-
-        DService.AddonLifecycle.RegisterListener(AddonEvent.PostSetup, BlacklistAddons, OnAddon);
-        DService.AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, BlacklistAddons, OnAddon);
-    }
-
-    private static void OnAddon(AddonEvent type, AddonArgs args)
-    {
-        IsBlocked = type switch
-        {
-            AddonEvent.PostSetup => true,
-            AddonEvent.PreFinalize => false,
-            _ => IsBlocked,
-        };
     }
 
     private static Utf8String* GetClipboardDataDetour(nint a1)
     {
-        if (IsBlocked || Framework.Instance()->WindowInactive) return GetClipboardDataHook.Original(a1);
+        if (Framework.Instance()->WindowInactive || IsAnyBlacklistAddonFocused()) return InvokeOriginal();
+        
+        var clipboardText = Clipboard.GetText();
+        if (string.IsNullOrWhiteSpace(clipboardText)) return InvokeOriginal();
 
-        var copyModule = Framework.Instance()->GetUIClipboard();
-        if (copyModule == null) return GetClipboardDataHook.Original(a1);
-
-        var originalText = Clipboard.GetText();
-        if (string.IsNullOrWhiteSpace(originalText)) return GetClipboardDataHook.Original(a1);
-
-        var modifiedText = originalText.Replace("\r\n", " ").Replace("\n", " ").Replace("\u000D", " ")
+        var modifiedText = clipboardText.Replace("\r\n", " ").Replace("\n", " ").Replace("\u000D", " ")
                                        .Replace("\u000D\u000A", " ");
+        if (modifiedText == clipboardText) return InvokeOriginal();
 
-        if (modifiedText == originalText) return GetClipboardDataHook.Original(a1);
+        var dest = (Utf8String*)(a1 + 8);
+        if (dest == null) return InvokeOriginal();
 
-        return Utf8String.FromString(modifiedText);
+        var sour = Utf8String.FromString(modifiedText);
+        
+        dest->Clear();
+        dest->Copy(sour);
+        
+        sour->Dtor(true);
+        
+        return dest;
+        
+        Utf8String* InvokeOriginal() => GetClipboardDataHook.Original(a1);
     }
 
-    public override void Uninit()
-    {
-        DService.AddonLifecycle.UnregisterListener(OnAddon);
-
-        base.Uninit();
-    }
+    private static bool IsAnyBlacklistAddonFocused()
+        => RaptureAtkModule.Instance()->RaptureAtkUnitManager.FocusedUnitsList.Entries
+                                                             .ToArray()
+                                                             .Where(x => x.Value != null)
+                                                             .Select(x => x.Value->NameString)
+                                                             .Where(x => !string.IsNullOrWhiteSpace(x))
+                                                             .ContainsAny(BlacklistAddons);
 }
