55183a5db5ec7f3b0e488c20a64960afeeb4e92e
AtmoOmen <atmoomen2004@outlook.com>
2025-03-17 20:41:54 +0800

    FastJoinAnotherPartyRecruitment

---
+++ UIOptimization/FastJoinAnotherPartyRecruitment.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using ClickLib;
+using DailyRoutines.Abstracts;
+using Dalamud.Game.Addon.Lifecycle;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+using Dalamud.Game.Gui.PartyFinder.Types;
+using Dalamud.Interface.Utility.Raii;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Client.UI.Info;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace DailyRoutines.Modules;
+
+public unsafe class FastJoinAnotherPartyRecruitment : DailyModuleBase
+{
+    public override ModuleInfo Info => new()
+    {
+        Title       = GetLoc("FastJoinAnotherPartyRecruitmentTitle"),
+        Description = GetLoc("FastJoinAnotherPartyRecruitmentDescription"),
+        Category    = ModuleCategories.UIOptimization
+    };
+
+    private static Dictionary<ulong, uint> CIDToListingID = [];
+    
+    public override void Init()
+    {
+        TaskHelper    ??= new() { TimeLimitMS = 10_000 };
+        Overlay       ??= new(this);
+        Overlay.Flags |=  ImGuiWindowFlags.NoBackground;
+        
+        CIDToListingID.Clear();
+        DService.PartyFinder.ReceiveListing += OnListing;
+        DService.AddonLifecycle.RegisterListener(AddonEvent.PostDraw,    "LookingForGroupDetail", OnAddon);
+        DService.AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "LookingForGroupDetail", OnAddon);
+        if (IsAddonAndNodesReady(LookingForGroupDetail)) OnAddon(AddonEvent.PostDraw, null);
+        
+        if (IsAddonAndNodesReady(LookingForGroup)) SendEvent(AgentId.LookingForGroup, 1, 17);
+    }
+
+    private void OnAddon(AddonEvent type, AddonArgs? args)
+    {
+        Overlay.IsOpen = type switch
+        {
+            AddonEvent.PostDraw   => true,
+            AddonEvent.PreFinalize => false,
+            _                      => Overlay.IsOpen
+        };
+    }
+
+    public override void OverlayUI()
+    {
+        var addon = LookingForGroupDetail;
+        if (addon == null)
+        {
+            Overlay.IsOpen = false;
+            return;
+        }
+
+        var buttonNode = addon->GetButtonNodeById(109);
+        if (buttonNode == null) return;
+
+        var windowSize = ImGui.GetWindowSize();
+        var nodeState  = NodeState.Get((AtkResNode*)buttonNode->OwnerNode);
+        
+        ImGui.SetWindowPos(new(nodeState.Position.X + ((nodeState.Size.X - windowSize.X) / 2), nodeState.Position.Y - windowSize.Y + 4f));
+        if (ImGui.Button(GetLoc("FastJoinAnotherPartyRecruitment-LeaveAndJoin")))
+            Enqueue();
+    }
+
+    private void OnListing(IPartyFinderListing listing, IPartyFinderListingEventArgs args) 
+        => CIDToListingID[listing.ContentId] = listing.Id;
+
+    private void Enqueue()
+    {
+        TaskHelper.Abort();
+        
+        var currentCID = AgentLookingForGroup.Instance()->ListingContentId;
+        if (currentCID == 0) return;
+
+        if (!CIDToListingID.TryGetValue(currentCID, out var currentListingID)) return;
+        
+        if (InfoProxyCrossRealm.IsCrossRealmParty() || DService.PartyList.Length >= 2)
+        {
+            TaskHelper.Enqueue(() =>
+            {
+                if (!Throttler.Throttle("FastJoinAnotherPartyRecruitment-Task", 100)) return false;
+                if (!InfoProxyCrossRealm.IsCrossRealmParty() && DService.PartyList.Length < 2) return true;
+                
+                ChatHelper.Instance.SendMessage("/leave");
+                return !InfoProxyCrossRealm.IsCrossRealmParty() && DService.PartyList.Length < 2;
+            });
+        }
+        
+        TaskHelper.Enqueue(() =>
+        {
+            if (!Throttler.Throttle("FastJoinAnotherPartyRecruitment-Task", 100)) return false;
+            if (AgentLookingForGroup.Instance()->ListingContentId == currentCID) return true;
+            
+            AgentLookingForGroup.Instance()->OpenListing(currentListingID);
+            return AgentLookingForGroup.Instance()->ListingContentId == currentCID;
+        });
+        
+        TaskHelper.Enqueue(() =>
+        {
+            if (!Throttler.Throttle("FastJoinAnotherPartyRecruitment-Task", 100)) return false;
+            
+            var buttonNode = LookingForGroupDetail->GetButtonNodeById(109);
+            if (buttonNode == null) return false;
+
+            buttonNode->ClickAddonButton(LookingForGroupDetail);
+            return true;
+        });
+        
+        TaskHelper.Enqueue(() => Click.TrySendClick("select_yes"));
+    }
+
+    public override void Uninit()
+    {
+        DService.AddonLifecycle.UnregisterListener(OnAddon);
+        DService.PartyFinder.ReceiveListing -= OnListing;
+        CIDToListingID.Clear();
+        
+        base.Uninit();
+    }
+}
