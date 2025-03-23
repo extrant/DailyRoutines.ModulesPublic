98fd914e7f0bf6433f88ec75876ab255b77bea6d
AtmoOmen <atmoomen2004@outlook.com>
2025-03-23 13:59:18 +0800

    让 -1 实现

--- UIOptimization/PartyFinderFilter.cs
+++ UIOptimization/PartyFinderFilter.cs
@@ -261,6 +261,8 @@ public class PartyFinderFilter : DailyModuleBase
 
         bool RoleCounter(int roleType, int maxCount, ClassJob currentJob)
         {
+            if (maxCount == -1) return true;
+            
             var count = 0;
             var hasSlot = false;
             
