e1c935ef683cde265e4dfcb605ce97e0fe04b50c
AtmoOmen <atmoomen2004@outlook.com>
2025-05-22 10:40:09 +0800

    自动召唤物跟随

--- Action/AutoPetFollow.cs
+++ Action/AutoPetFollow.cs
@@ -1,20 +1,24 @@
+using System.Collections.Generic;
 using DailyRoutines.Abstracts;
 using DailyRoutines.Infos;
 using DailyRoutines.Managers;
 using Dalamud.Game.ClientState.Conditions;
 using FFXIVClientStructs.FFXIV.Client.Game.Character;
+using FFXIVClientStructs.FFXIV.Client.Game.Control;
 
-namespace DailyRoutines.Modules;
+namespace DailyRoutines.ModulesPublic;
 
 public class AutoPetFollow : DailyModuleBase
 {
     public override ModuleInfo Info { get; } = new()
     {
-        Title = GetLoc("AutoPetFollowTitle"),
+        Title       = GetLoc("AutoPetFollowTitle"),
         Description = GetLoc("AutoPetFollowDescription"),
-        Category = ModuleCategories.Action,
+        Category    = ModuleCategories.Action,
     };
 
+    private static readonly HashSet<uint> ValidClassJobs = [26, 27, 28];
+    
     private static Config ModuleConfig = null!;
     
     public override void Init()
@@ -32,12 +36,18 @@ public class AutoPetFollow : DailyModuleBase
 
     private static unsafe void OnConditionChanged(ConditionFlag flag, bool value)
     {
-        if (flag is not ConditionFlag.InCombat || value) return;
+        if (flag != ConditionFlag.InCombat            ||
+            value                                     ||
+            GameState.IsInPVPArea                     ||
+            DService.Condition[ConditionFlag.Mounted] ||
+            !ValidClassJobs.Contains(GameState.ClassJob))
+            return;
 
-        if (DService.ClientState.LocalPlayer is not { ClassJob.RowId: 26 or 27 or 28 } player) return;
+        var localPlayer   = Control.GetLocalPlayer();
+        if (localPlayer == null) return;
 
-        var isPetSummoned = CharacterManager.Instance()->LookupPetByOwnerObject((BattleChara*)player.Address) != null;
-        if (!isPetSummoned) return;
+        var pet = CharacterManager.Instance()->LookupPetByOwnerObject(localPlayer);
+        if (pet == null || !pet->GetIsTargetable()) return;
 
         ExecuteCommandManager.ExecuteCommandComplex(ExecuteCommandComplexFlag.PetAction, 0xE0000000, 2);
 
@@ -45,10 +55,8 @@ public class AutoPetFollow : DailyModuleBase
             NotificationInfo(GetLoc("AutoPetFollow-Notification"));
     }
 
-    public override void Uninit()
-    {
+    public override void Uninit() => 
         DService.Condition.ConditionChange -= OnConditionChanged;
-    }
 
     public class Config : ModuleConfiguration
     {
