66dba2955cc937fd5fbf40dde069da574ddef27b
AtmoOmen <atmoomen2004@outlook.com>
2025-06-15 22:03:07 +0800

    AutoNotifyDiademWeather

---
+++ Notice/AutoNotifyDiademWeather.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DailyRoutines.Abstracts;
+using DailyRoutines.Infos;
+using DailyRoutines.Managers;
+using Dalamud.Plugin.Services;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using Lumina.Excel.Sheets;
+
+namespace DailyRoutines.ModulesPublic;
+
+public class AutoNotifyDiademWeather : DailyModuleBase
+{
+    public override ModuleInfo Info { get; } = new()
+    {
+        Title       = GetLoc("AutoNotifyDiademWeatherTitle"),
+        Description = GetLoc("AutoNotifyDiademWeatherDescription"),
+        Category    = ModuleCategories.Notice
+    };
+
+    private static readonly List<uint> SpecialWeathers = [133, 134, 135, 136];
+    
+    private static Config ModuleConfig = null!;
+
+    private static uint LastWeather;
+    
+    public override void Init()
+    {
+        ModuleConfig = LoadConfig<Config>() ?? new();
+        
+        DService.ClientState.TerritoryChanged += OnZoneChanged;
+    }
+
+    public override void ConfigUI()
+    {
+        ImGui.TextColored(LightSkyBlue, LuminaWrapper.GetAddonText(8555));
+        
+        var weathers = string.Join(',',
+                                   ModuleConfig.Weathers
+                                               .Select(x => LuminaGetter.GetRow<Weather>(x)?.Name.ExtractText() ?? string.Empty)
+                                               .Distinct());
+        using var combo = ImRaii.Combo("###WeathersCombo", weathers, ImGuiComboFlags.HeightLarge);
+        if (combo)
+        {
+            foreach (var weather in SpecialWeathers)
+            {
+                if (!LuminaGetter.TryGetRow<Weather>(weather, out var data)) continue;
+                if (!DService.Texture.TryGetFromGameIcon(new((uint)data.Icon), out var icon)) continue;
+
+                if (ImGuiOm.SelectableImageWithText(icon.GetWrapOrEmpty().ImGuiHandle,
+                                                    new(ImGui.GetTextLineHeightWithSpacing()), $"{data.Name.ExtractText()}",
+                                                    ModuleConfig.Weathers.Contains(weather),
+                                                    ImGuiSelectableFlags.DontClosePopups))
+                {
+                    if (!ModuleConfig.Weathers.Add(weather))
+                        ModuleConfig.Weathers.Remove(weather);
+
+                    SaveConfig(ModuleConfig);
+                }
+            }
+        }
+    }
+
+    private static void OnZoneChanged(ushort zone)
+    {
+        FrameworkManager.Unregister(OnUpdate);
+        
+        zone = (ushort)GameState.TerritoryType;
+        if (zone != 939) return;
+
+        FrameworkManager.Register(OnUpdate, throttleMS: 10_000);
+    }
+
+    private static unsafe void OnUpdate(IFramework framework)
+    {
+        var weatherID = WeatherManager.Instance()->GetCurrentWeather();
+        if (LastWeather == weatherID || !LuminaGetter.TryGetRow<Weather>(weatherID, out var weather)) return;
+        
+        LastWeather = weatherID;
+        if (!ModuleConfig.Weathers.Contains(weatherID)) return;
+
+        var message = GetLoc("AutoNotifyDiademWeather-Notification", weather.Name.ExtractText());
+        Chat(message);
+        NotificationInfo(message);
+    }
+
+    public override void Uninit()
+    {
+        DService.ClientState.TerritoryChanged -= OnZoneChanged;
+        FrameworkManager.Unregister(OnUpdate);
+
+        LastWeather = 0;
+    }
+
+    private class Config : ModuleConfiguration
+    {
+        public HashSet<uint> Weathers = [];
+    }
+}
