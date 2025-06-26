43e9a35ef052cbab8d8a5cfd50579ced621f6c8e
AtmoOmen <atmoomen2004@outlook.com>
2025-06-26 21:19:50 +0800

    AutoInventoryTransfer

--- UIOperation/AutoInventoryTransfer.cs
+++ UIOperation/AutoInventoryTransfer.cs
@@ -1,12 +1,6 @@
-﻿using DailyRoutines.Abstracts;
-using DailyRoutines.Managers;
-using Dalamud.Game.Addon.Lifecycle;
-using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
-using Dalamud.Game.ClientState.Keys;
+﻿using System.Collections.Generic;
+using DailyRoutines.Abstracts;
 using Dalamud.Game.Gui.ContextMenu;
-using FFXIVClientStructs.FFXIV.Component.GUI;
-using System.Collections.Generic;
-using static FFXIVClientStructs.STD.Helper.IStaticEncoding;
 
 namespace DailyRoutines.ModulesPublic;
 
@@ -14,87 +8,50 @@ public unsafe class AutoInventoryTransfer : DailyModuleBase
 {
     public override ModuleInfo Info { get; } = new()
     {
-        Title = GetLoc("AutoInventoryTransfer"),
+        Title       = GetLoc("AutoInventoryTransferTitle"),
         Description = GetLoc("AutoInventoryTransferDescription"),
-        Category = ModuleCategories.UIOperation,
-        Author = ["Yangdoubao"]
+        Category    = ModuleCategories.UIOperation,
+        Author      = ["Yangdoubao"]
     };
 
-    private readonly string[] entrustTexts;
-    public AutoInventoryTransfer()
-    {
-        entrustTexts = GetMenuItems();
-    }
-    private string[] GetMenuItems()
-    {
-
-        var addonIds = new uint[]
-        {
-            97, 98, 881, 887
-        };
-        var menuTexts = new List<string>();
-        foreach (var id in addonIds)
-        {
-            var text = LuminaWrapper.GetAddonText(id);
-            if (!string.IsNullOrEmpty(text))
-            {
-                menuTexts.Add(text);
-            }
-        }
-        return [.. menuTexts];
-    }
+    private static readonly List<string> MenuTexts =
+    [
+        LuminaWrapper.GetAddonText(97),
+        LuminaWrapper.GetAddonText(98),
+        LuminaWrapper.GetAddonText(881),
+        LuminaWrapper.GetAddonText(887)
+    ];
 
     public override void Init()
     {
         TaskHelper ??= new() { TimeLimitMS = 2_000 };
+
         DService.ContextMenu.OnMenuOpened += OnContextMenuOpened;
     }
 
+    public override void ConfigUI() => ConflictKeyText();
+
     private void OnContextMenuOpened(IMenuOpenedArgs args)
     {
-        if (!IsConflictKeyPressed()) return;
-        if (IsInventoryOpen())
-        {
-            HandleTransfer();
-            return;
-        }
+        if (!IsConflictKeyPressed() || !IsInventoryOpen()) return;
+        
+        TaskHelper.Enqueue(() => IsAddonAndNodesReady(ContextMenu));
+        TaskHelper.Enqueue(() => { ClickContextMenu(MenuTexts); });
+        
         return;
 
         bool IsInventoryOpen()
-            => IsAddonAndNodesReady(Inventory) ||
-               IsAddonAndNodesReady(InventoryLarge) ||
+            => IsAddonAndNodesReady(Inventory)          ||
+               IsAddonAndNodesReady(InventoryLarge)     ||
                IsAddonAndNodesReady(InventoryExpansion) ||
-               IsAddonAndNodesReady(InventoryRetainer) ||
+               IsAddonAndNodesReady(InventoryRetainer)  ||
                IsAddonAndNodesReady(InventoryRetainerLarge);
-
-    }
-
-    private bool HandleTransfer()
-    {
-        if (!IsAddonAndNodesReady(InfosOm.ContextMenu))
-        {
-            foreach (var text in entrustTexts)
-            {
-                if (ClickContextMenu(text))
-                    return true;
-            }
-        }
-        else
-        {
-            TaskHelper.DelayNext(10);
-            TaskHelper.Enqueue(() =>
-            {
-                HandleTransfer();
-            });
-        }
-            
-
-        return true;
     }
 
     public override void Uninit()
     {
         DService.ContextMenu.OnMenuOpened -= OnContextMenuOpened;
+        
         base.Uninit();
     }
 }
