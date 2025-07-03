6fcbf88023d90834ef17d1523ba64f57fc26c497
AtmoOmen <atmoomen2004@outlook.com>
2025-07-03 11:12:40 +0800

    加入职业切换判断

--- Action/AutoCancelStarContributor.cs
+++ Action/AutoCancelStarContributor.cs
@@ -20,19 +20,27 @@ public unsafe class AutoCancelStarContributor : DailyModuleBase
     public override void Init() => 
         DService.ClientState.TerritoryChanged += OnZoneChanged;
 
-    private static void OnZoneChanged(ushort zone)
-    {
-        FrameworkManager.Unregister(OnUpdate);
-        if (GameState.TerritoryIntendedUse != 60) return;
-        
-        FrameworkManager.Register(OnUpdate, throttleMS: 10_000);
-    }
-
     public override void Uninit()
     {
         DService.ClientState.TerritoryChanged -= OnZoneChanged;
+        DService.ClientState.ClassJobChanged  -= OnClassJobChanged;
+        
         FrameworkManager.Unregister(OnUpdate);
     }
+    
+    private static void OnZoneChanged(ushort zone)
+    {
+        FrameworkManager.Unregister(OnUpdate);
+        DService.ClientState.ClassJobChanged -= OnClassJobChanged;
+        
+        if (GameState.TerritoryIntendedUse != 60) return;
+        
+        FrameworkManager.Register(OnUpdate, throttleMS: 10_000);
+        DService.ClientState.ClassJobChanged  += OnClassJobChanged;
+    }
+    
+    private static void OnClassJobChanged(uint classJobID) => 
+        OnUpdate(DService.Framework);
 
     private static void OnUpdate(IFramework framework)
     {
