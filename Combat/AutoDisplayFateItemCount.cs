576b33900749ba683195f119bf4a82ef9f4ae2cd
AtmoOmen <atmoomen2004@outlook.com>
2025-07-16 17:39:03 +0800

    AutoDisplayFateItemCount

---
+++ Combat/AutoDisplayFateItemCount.cs
@@ -0,0 +1,79 @@
+﻿using System.Numerics;
+using DailyRoutines.Abstracts;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.Game.Fate;
+using Lumina.Excel.Sheets;
+
+namespace DailyRoutines.ModulesPublic;
+
+public unsafe class AutoDisplayFateItemCount : DailyModuleBase
+{
+    public override ModuleInfo Info { get; } = new()
+    {
+        Title       = GetLoc("AutoDisplayFateItemCountTitle"),
+        Description = GetLoc("AutoDisplayFateItemCountDescription"),
+        Category    = ModuleCategories.Combat
+    };
+
+    public override void Init()
+    {
+        Overlay       ??= new(this);
+        Overlay.Flags |=  ImGuiWindowFlags.NoBackground | ImGuiWindowFlags.NoTitleBar;
+        
+        GameState.EnterFate += OnEnterFate;
+        if (FateManager.Instance()->CurrentFate != null)
+            OnEnterFate(0);
+    }
+
+    public override void Uninit()
+    {
+        GameState.EnterFate -= OnEnterFate;
+        
+        base.Uninit();
+    }
+
+    private void OnEnterFate(uint _) => Overlay.IsOpen = true;
+
+    public override void OverlayUI()
+    {
+        var currentFate = FateManager.Instance()->CurrentFate;
+        if (currentFate == null                                                  ||
+            !LuminaGetter.TryGetRow<Fate>(currentFate->FateId, out var fateData) ||
+            fateData.EventItem.RowId == 0)
+        {
+            Overlay.IsOpen = false;
+            return;
+        }
+
+        if (!DService.Texture.TryGetFromGameIcon(new(fateData.EventItem.Value.Icon), out var texture) ||
+            !IsAddonAndNodesReady(ToDoList)) 
+            return;
+        
+        ImGui.SetWindowPos(new Vector2(ToDoList->X, ToDoList->Y));
+        
+        ImGui.Image(texture.GetWrapOrEmpty().ImGuiHandle, new(ImGui.GetTextLineHeightWithSpacing()));
+        
+        ImGui.SameLine();
+        ImGui.AlignTextToFramePadding();
+        ImGuiOm.TextOutlined(Orange, $"{fateData.EventItem.Value.Singular}");
+
+        ImGui.Spacing();
+        
+        using var table = ImRaii.Table("##Table", 2);
+        if (!table) return;
+        
+        ImGui.TableNextRow();
+        ImGui.TableNextColumn();
+        ImGuiOm.TextOutlined(White, $"{GetLoc("AutoDisplayFateItemCount-HoldCount")}:");
+
+        ImGui.TableNextColumn();
+        ImGuiOm.TextOutlined(White, $"{InventoryManager.Instance()->GetInventoryItemCount(fateData.EventItem.RowId)}");
+
+        ImGui.TableNextRow();
+        ImGui.TableNextColumn();
+        ImGuiOm.TextOutlined(White, $"{GetLoc("AutoDisplayFateItemCount-HandInCount")}:");
+
+        ImGui.TableNextColumn();
+        ImGuiOm.TextOutlined(White, $"{currentFate->HandInCount}");
+    }
+}
