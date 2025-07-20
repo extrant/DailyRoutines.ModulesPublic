3ab76a7410ff03407a264afc917f6a8fd786d972
AtmoOmen <atmoomen2004@outlook.com>
2025-07-20 21:54:36 +0800

    AutoBlockTitleMovie

---
+++ System/AutoBlockTitleMovie.cs
@@ -0,0 +1,31 @@
+﻿using DailyRoutines.Abstracts;
+using Dalamud.Game.Addon.Lifecycle;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+namespace DailyRoutines.ModulesPublic;
+
+public class AutoBlockTitleMovie : DailyModuleBase
+{
+    public override ModuleInfo Info { get; } = new()
+    {
+        Title       = GetLoc("AutoBlockTitleMovieTitle"),
+        Description = GetLoc("AutoBlockTitleMovieDescription"),
+        Category    = ModuleCategories.System
+    };
+    
+    public override void Init() => 
+        DService.AddonLifecycle.RegisterListener(AddonEvent.PostDraw, "Title", OnAddon);
+
+    // 非即时的懒得 Hook 了
+    private static unsafe void OnAddon(AddonEvent type, AddonArgs args)
+    {
+        var agent = AgentLobby.Instance();
+        if (agent == null) return;
+
+        agent->IdleTime = 0;
+    }
+    
+    public override void Uninit() => 
+        DService.AddonLifecycle.UnregisterListener(OnAddon);
+}
