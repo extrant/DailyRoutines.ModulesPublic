4026d3092f28721c6101d2718ab9f11a2f923654
AtmoOmen <atmoomen2004@outlook.com>
2025-07-20 19:58:06 +0800

    格式更新

--- System/AutoBlockShutdownFromLobbyError.cs
+++ System/AutoBlockShutdownFromLobbyError.cs
@@ -2,7 +2,7 @@
 using Dalamud.Hooking;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 
-namespace DailyRoutines.Modules;
+namespace DailyRoutines.ModulesPublic;
 
 public unsafe class AutoBlockShutdownFromLobbyError : DailyModuleBase
 {
@@ -13,15 +13,13 @@ public unsafe class AutoBlockShutdownFromLobbyError : DailyModuleBase
         Category    = ModuleCategories.System
     };
 
-    private static readonly CompSig AtkMessageBoxReceiveEventSig =
-        new("40 53 48 83 EC 30 48 8B D9 49 8B C8 E8 ?? ?? ?? ?? 8B D0");
-    private delegate bool AtkMessageBoxReceiveEventDelegate(AtkMessageBoxManager* manager, nint a2, AtkValue* values);
-    private static Hook<AtkMessageBoxReceiveEventDelegate>? AtkMessageBoxReceiveEventHook;
+    private static readonly CompSig                                  AtkMessageBoxReceiveEventSig = new("40 53 48 83 EC 30 48 8B D9 49 8B C8 E8 ?? ?? ?? ?? 8B D0");
+    private delegate        bool                                     AtkMessageBoxReceiveEventDelegate(AtkMessageBoxManager* manager, nint a2, AtkValue* values);
+    private static          Hook<AtkMessageBoxReceiveEventDelegate>? AtkMessageBoxReceiveEventHook;
     
     public override void Init()
     {
-        AtkMessageBoxReceiveEventHook ??=
-            AtkMessageBoxReceiveEventSig.GetHook<AtkMessageBoxReceiveEventDelegate>(AtkMessageBoxReceiveEventDetour);
+        AtkMessageBoxReceiveEventHook ??= AtkMessageBoxReceiveEventSig.GetHook<AtkMessageBoxReceiveEventDelegate>(AtkMessageBoxReceiveEventDetour);
         AtkMessageBoxReceiveEventHook.Enable();
     }
 
