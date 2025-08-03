79f339a3c3269a449452eb0051f6b8032e6ef894
AtmoOmen <atmoomen2004@outlook.com>
2025-08-03 23:52:11 +0800

    AutoHideExpBar

---
+++ UIOptimization/AutoHideExpBar.cs
@@ -0,0 +1,34 @@
+﻿using DailyRoutines.Abstracts;
+using Dalamud.Hooking;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace DailyRoutines.ModulesPublic;
+
+public unsafe class AutoHideExpBar : DailyModuleBase
+{
+    public override ModuleInfo Info { get; } = new()
+    {
+        Title       = GetLoc("AutoHideExpBarTitle"),
+        Description = GetLoc("AutoHideExpBarDescription"),
+        Category    = ModuleCategories.UIOptimization
+    };
+    
+    private static readonly CompSig UpdateExpSig = new("48 8B C4 4C 89 48 20 4C 89 40 18 53");
+    private delegate void UpdateExpDelegate(AgentHUD* agent, NumberArrayData* expNumberArray, StringArrayData* expStringArray, StringArrayData* characterStringArray);
+    private static Hook<UpdateExpDelegate>? UpdateExpHook;
+    
+    protected override void Init()
+    {
+        UpdateExpHook = UpdateExpSig.GetHook<UpdateExpDelegate>(UpdateExpDetour);
+        UpdateExpHook.Enable();
+    }
+
+    private static void UpdateExpDetour(AgentHUD* agent, NumberArrayData* expNumberArray, StringArrayData* expStringArray, StringArrayData* characterStringArray)
+    {
+        UpdateExpHook.Original(agent, expNumberArray, expStringArray, characterStringArray);
+
+        if (Exp != null)
+            Exp->IsVisible = !agent->ExpFlags.HasFlag(AgentHudExpFlag.MaxLevel);
+    }
+}
