f7e0909a0a5eeea70d462c2873030a917f3f0733
AtmoOmen <atmoomen2004@outlook.com>
2025-08-10 15:36:47 +0800

    RealPositionInNaviMap

---
+++ UIOptimization/RealPositionInNaviMap.cs
@@ -0,0 +1,108 @@
+﻿using DailyRoutines.Abstracts;
+using Dalamud.Game.Addon.Lifecycle;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Classes;
+using KamiToolKit.Nodes;
+
+namespace DailyRoutines.ModulesPublic;
+
+public class RealPositionInNaviMap : DailyModuleBase
+{
+    public override ModuleInfo Info { get; } = new()
+    {
+        Title       = GetLoc("RealPositionInNaviMapTitle"),
+        Description = GetLoc("RealPositionInNaviMapDescription"),
+        Category    = ModuleCategories.UIOptimization
+    };
+
+    private static Config ModuleConfig = null!;
+    
+    private static TextButtonNode? PositionButton;
+    
+    protected override void Init()
+    {
+        ModuleConfig = LoadConfig<Config>() ?? new();
+        
+        DService.AddonLifecycle.RegisterListener(AddonEvent.PostDraw,    "_NaviMap", OnAddon);
+        DService.AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "_NaviMap", OnAddon);
+    }
+
+    protected override void ConfigUI()
+    {
+        ImGui.TextColored(LightSkyBlue, GetLoc("RealPositionInNaviMap-CopyFormat"));
+        ImGuiOm.HelpMarker(GetLoc("RealPositionInNaviMap-CopyFormatHelp"), 20f * GlobalFontScale);
+
+        ImGui.InputText("###CopyFormat", ref ModuleConfig.CopyFormat, 256);
+        if (ImGui.IsItemDeactivatedAfterEdit())
+            SaveConfig(ModuleConfig);
+    }
+
+    protected override void Uninit()
+    {
+        DService.AddonLifecycle.UnregisterListener(OnAddon);
+        OnAddon(AddonEvent.PreFinalize, null);
+    }
+
+    private static unsafe void OnAddon(AddonEvent type, AddonArgs args)
+    {
+        switch (type)
+        {
+            case AddonEvent.PreFinalize:
+                Service.AddonController.DetachNode(PositionButton);
+                PositionButton = null;
+                break;
+            case AddonEvent.PostDraw:
+                if (NaviMap == null) return;
+
+                if (PositionButton == null)
+                {
+                    PositionButton = new()
+                    {
+                        Position  = new(0),
+                        Size      = new(130, 18),
+                        IsVisible = true,
+                        Label     = string.Empty,
+                        OnClick   = () =>
+                        {
+                            if (DService.ObjectTable.LocalPlayer is not { } player) return;
+
+                            var agent = AgentMap.Instance();
+                            agent->SetFlagMapMarker(GameState.TerritoryType, GameState.Map, player.Position);
+                            
+                            var result = string.Format(ModuleConfig.CopyFormat,
+                                                       player.Position.X,
+                                                       player.Position.Y,
+                                                       player.Position.Z);
+                            if (!string.IsNullOrWhiteSpace(result))
+                            {
+                                ImGui.SetClipboardText(result);
+                                NotificationSuccess($"{GetLoc("CopiedToClipboard")}: {result}");
+                            }
+                        }
+                    };
+
+                    PositionButton.BackgroundNode.IsVisible = false;
+                    
+                    PositionButton.LabelNode.TextFlags        = TextFlags.Glare;
+                    PositionButton.LabelNode.TextColor        = ColorHelper.GetColor(8);
+                    PositionButton.LabelNode.TextOutlineColor = new(0, 0, 0, 1);
+                    
+                    Service.AddonController.AttachNode(PositionButton, NaviMap->GetNodeById(5));
+
+                    NaviMap->GetTextNodeById(6)->ToggleVisibility(false);
+                }
+
+                if (LocalPlayerState.IsMoving && DService.ObjectTable.LocalPlayer is { } localPlayer)
+                    PositionButton.Label = $"X:{localPlayer.Position.X:F1} Y:{localPlayer.Position.Y:F1} Z:{localPlayer.Position.Z:F1}";
+                
+                break;
+        }
+    }
+
+    private class Config : ModuleConfiguration
+    {
+        public string CopyFormat = @"X:{0:F1} Y:{1:F1} Z:{2:F1}";
+    }
+}
