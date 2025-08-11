7f4d5e9f34e90b8c6077c55334b477f08598386a
AtmoOmen <atmoomen2004@outlook.com>
2025-08-12 00:53:13 +0800

    OptimizedTargetInfo

---
+++ UIOptimization/OptimizedTargetInfo.cs
@@ -0,0 +1,431 @@
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using DailyRoutines.Abstracts;
+using Dalamud.Game.Addon.Lifecycle;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Nodes;
+using KamiToolKit.Extensions;
+
+namespace DailyRoutines.ModulesPublic;
+
+public unsafe class OptimizedTargetInfo : DailyModuleBase
+{
+    public override ModuleInfo Info { get; } = new()
+    {
+        Title       = "目标情报界面优化",
+        Description = "对目标情报与焦点目标情报界面进行优化, 显示目标体力值与目标技能咏唱剩余时间, 放大自身状态效果, 并支持快捷取消焦点目标",
+        Category    = ModuleCategories.UIOptimization,
+    };
+
+    private static Config ModuleConfig = null!;
+    
+    private static TextNode? TargetHPTextNode;
+    private static TextNode? FocusTargetHPTextNode;
+    private static TextNode? MainTargetSplitHPTextNode;
+    
+    private static int NumberPreview = 12345678;
+    
+    protected override void Init()
+    {
+        ModuleConfig = LoadConfig<Config>() ?? new();
+
+        DService.AddonLifecycle.RegisterListener(AddonEvent.PostDraw,    "_TargetInfo", OnAddonTargetInfo);
+        DService.AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "_TargetInfo", OnAddonTargetInfo);
+        
+        DService.AddonLifecycle.RegisterListener(AddonEvent.PostDraw,    "_TargetInfoMainTarget", OnAddonTargetInfoSplitTarget);
+        DService.AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "_TargetInfoMainTarget", OnAddonTargetInfoSplitTarget);
+        
+        DService.AddonLifecycle.RegisterListener(AddonEvent.PostDraw,    "_FocusTargetInfo", OnAddonFocusTargetInfo);
+        DService.AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "_FocusTargetInfo", OnAddonFocusTargetInfo);
+    }
+
+    protected override void ConfigUI()
+    {
+        ImGui.AlignTextToFramePadding();
+        ImGui.TextColored(LightSkyBlue, $"{GetLoc("OptimizedTargetInfo-DisplayFormat")}");
+        
+        ImGui.SetNextItemWidth(400f * GlobalFontScale);
+        using (ImRaii.PushIndent())
+        using (var combo = ImRaii.Combo("###DisplayFormatCombo",
+                                        $"{DisplayFormatLoc.GetValueOrDefault(ModuleConfig.DisplayFormat, GetLoc("OptimizedTargetInfo-UnknownDisplayFormat"))} " +
+                                        $"({FormatNumber((uint)NumberPreview, ModuleConfig.DisplayFormat)})", 
+                                        ImGuiComboFlags.HeightLarge))
+        {
+            if (combo)
+            {
+                ImGui.AlignTextToFramePadding();
+                ImGui.Text($"{GetLoc("OptimizedTargetInfo-NumberPreview")}:");
+
+                ImGui.SameLine();
+                ImGui.SetNextItemWidth(-1f);
+                if (ImGui.InputInt("###PreviewNumberInput", ref NumberPreview, 0, 0))
+                    NumberPreview = (int)Math.Clamp(NumberPreview, 0, uint.MaxValue);
+
+                ImGui.Separator();
+                ImGui.Spacing();
+
+                foreach (var displayFormat in Enum.GetValues<DisplayFormat>())
+                {
+                    if (ImGui.Selectable($"{DisplayFormatLoc.GetValueOrDefault(displayFormat, GetLoc("OptimizedTargetInfo-UnknownDisplayFormat"))} " +
+                                         $"({FormatNumber((uint)NumberPreview, displayFormat)})##FormatSelect",
+                                         ModuleConfig.DisplayFormat == displayFormat))
+                    {
+                        ModuleConfig.DisplayFormat = displayFormat;
+                        SaveConfig(ModuleConfig);
+                    }
+                }
+            }
+        }
+            
+        ImGui.AlignTextToFramePadding();
+        ImGui.TextColored(LightSkyBlue, $"{GetLoc("OptimizedTargetInfo-DisplayStringFormat")}");
+        
+        using (ImRaii.PushIndent())
+        {
+            ImGui.SetNextItemWidth(400f * GlobalFontScale);
+            ImGui.InputText("###DisplayStringFormatInput", ref ModuleConfig.DisplayFormatString, 128);
+            if (ImGui.IsItemDeactivatedAfterEdit())
+                SaveConfig(ModuleConfig);
+            ImGuiOm.HelpMarker(GetLoc("OptimizedTargetInfo-DisplayStringFormatHelp"));
+        }
+
+        ImGui.NewLine();
+        
+        // 目标
+        DrawTargetConfigSection(LuminaWrapper.GetAddonText(1030),
+                                "Target",
+                                ref ModuleConfig.AlignLeft,
+                                ref ModuleConfig.Position,
+                                ref ModuleConfig.CustomColor,
+                                ref ModuleConfig.FontSize,
+                                ref ModuleConfig.HideAutoAttack,
+                                true,
+                                ref ModuleConfig.IsEnabled);
+        
+        ImGui.NewLine();
+        
+        // 焦点目标
+        DrawTargetConfigSection(LuminaWrapper.GetAddonText(1110),
+                                "Focus",
+                                ref ModuleConfig.FocusAlignLeft,
+                                ref ModuleConfig.FocusPosition,
+                                ref ModuleConfig.FocusCustomColor,
+                                ref ModuleConfig.FocusFontSize,
+                                ref ModuleConfig.HideAutoAttack,
+                                false,
+                                ref ModuleConfig.FocusIsEnabled);
+    }
+
+    private void DrawTargetConfigSection(
+        string      sectionTitle,
+        string      prefix,
+        ref bool    alignLeft,
+        ref Vector2 position,
+        ref Vector4 customColor,
+        ref byte    fontSize,
+        ref bool    hideAutoAttack,
+        bool        showHideAutoAttack,
+        ref bool    isEnabled)
+    {
+        using var id = ImRaii.PushId($"{prefix}_{sectionTitle}");
+
+        ImGui.AlignTextToFramePadding();
+        ImGui.TextColored(LightSkyBlue, sectionTitle);
+
+        ImGui.SameLine(0, 8f * GlobalFontScale);
+        if (ImGui.Checkbox($"{GetLoc("Enable")}", ref isEnabled))
+            SaveConfig(ModuleConfig);
+
+        if (!isEnabled) return;
+        
+        ImGui.Spacing();
+
+        using var indent = ImRaii.PushIndent();
+
+        if (ImGui.Checkbox($"{GetLoc("OptimizedTargetInfo-AlignLeft")}###AlignLeft", ref alignLeft))
+            SaveConfig(ModuleConfig);
+        
+        if (ImGui.ColorButton($"###{prefix}CustomColorButton", customColor))
+            ImGui.OpenPopup($"{prefix}CustomColorPopup");
+        ImGuiOm.TooltipHover(GetLoc("OptimizedTargetInfo-ZeroAlphaHelp"));
+        
+        ImGui.SameLine();
+        ImGui.Text($"{GetLoc("OptimizedTargetInfo-CustomColor")}");
+
+        using (var popup = ImRaii.Popup($"{prefix}CustomColorPopup"))
+        {
+            if (popup)
+            {
+                ImGui.ColorPicker4($"###{prefix}CustomColor", ref customColor);
+                if (ImGui.IsItemDeactivatedAfterEdit())
+                    SaveConfig(ModuleConfig);
+            }
+        }
+        
+        if (showHideAutoAttack)
+        {
+            if (ImGui.Checkbox($"{GetLoc("OptimizedTargetInfo-HideAutoAttackIcon")}###{prefix}HideAutoAttackIcon", ref hideAutoAttack))
+                SaveConfig(ModuleConfig);
+        }
+        
+        ImGui.SetNextItemWidth(150f * GlobalFontScale);
+        ImGui.InputFloat2($"{GetLoc("OptimizedTargetInfo-PosOffset")}###Position", ref position, "%.2f");
+        if (ImGui.IsItemDeactivatedAfterEdit())
+            SaveConfig(ModuleConfig);
+
+        var fontSizeInt = (int)fontSize;
+        ImGui.SetNextItemWidth(150f * GlobalFontScale);
+        if (ImGui.SliderInt($"{GetLoc("FontScale")}###FontSize", ref fontSizeInt, 1, 32))
+            fontSize = (byte)fontSizeInt;
+        if (ImGui.IsItemDeactivatedAfterEdit())
+            SaveConfig(ModuleConfig);
+    }
+
+    private static void OnAddonTargetInfo(AddonEvent type, AddonArgs args) =>
+        HandleAddonEventTargetInfo(type,
+                                   ModuleConfig.IsEnabled,
+                                   ModuleConfig.HideAutoAttack,
+                                   18,
+                                   TargetInfo,
+                                   ref TargetHPTextNode,
+                                   "OptimizedTargetInfo-MainTarget",
+                                   16,
+                                   19,
+                                   ModuleConfig.Position,
+                                   ModuleConfig.AlignLeft,
+                                   ModuleConfig.FontSize,
+                                   ModuleConfig.CustomColor,
+                                   () => (DService.Targets.SoftTarget ?? DService.Targets.Target) as IBattleChara,
+                                   (width, height) => new Vector2(width - 5, height + 2));
+
+    private static void OnAddonTargetInfoSplitTarget(AddonEvent type, AddonArgs args) =>
+        HandleAddonEventTargetInfo(type,
+                                   ModuleConfig.IsEnabled,
+                                   ModuleConfig.HideAutoAttack,
+                                   12,
+                                   TargetInfoMainTarget,
+                                   ref MainTargetSplitHPTextNode,
+                                   "OptimizedTargetInfo-MainTargetSplit",
+                                   10,
+                                   13,
+                                   ModuleConfig.Position,
+                                   ModuleConfig.AlignLeft,
+                                   ModuleConfig.FontSize,
+                                   ModuleConfig.CustomColor,
+                                   () => (DService.Targets.SoftTarget ?? DService.Targets.Target) as IBattleChara,
+                                   (width, height) => new Vector2(width - 5, height + 2));
+
+    private static void OnAddonFocusTargetInfo(AddonEvent type, AddonArgs args) =>
+        HandleAddonEventTargetInfo(type,
+                                   ModuleConfig.FocusIsEnabled,
+                                   false,
+                                   0,
+                                   FocusTargetInfo,
+                                   ref FocusTargetHPTextNode,
+                                   "OptimizedTargetInfo-FocusTarget",
+                                   10,
+                                   18,
+                                   ModuleConfig.FocusPosition,
+                                   ModuleConfig.FocusAlignLeft,
+                                   ModuleConfig.FocusFontSize,
+                                   ModuleConfig.FocusCustomColor,
+                                   () => DService.Targets.FocusTarget as IBattleChara,
+                                   (width, height) => new Vector2(width - 5, height + 2));
+
+    private static void HandleAddonEventTargetInfo(
+        AddonEvent                type,
+        bool                      isEnabled,
+        bool                      isHideAutoAttack,
+        uint                      autoAttackNodeID,
+        AtkUnitBase*              addon,
+        ref TextNode?             textNode,
+        string                    throttleKey,
+        uint                      textNodeID,
+        uint                      gaugeNodeID,
+        Vector2                   position,
+        bool                      alignLeft,
+        byte                      fontSize,
+        Vector4                   customColor,
+        Func<IBattleChara?>       getTarget,
+        Func<uint, uint, Vector2> getSizeFunc)
+    {
+        switch (type)
+        {
+            case AddonEvent.PostDraw:
+                if (addon == null) return;
+
+                if (textNode == null)
+                {
+                    var sourceTextNode = addon->GetTextNodeById(textNodeID);
+                    if (sourceTextNode == null) return;
+
+                    var gauge = addon->GetComponentByNodeId(gaugeNodeID);
+                    if (gauge == null) return;
+
+                    textNode = new()
+                    {
+                        IsVisible        = isEnabled,
+                        Position         = position,
+                        AlignmentType    = alignLeft ? AlignmentType.BottomLeft : AlignmentType.BottomRight,
+                        FontSize         = fontSize,
+                        TextFlags        = TextFlags.Edge | TextFlags.Bold,
+                        TextColor        = customColor.W != 0 ? customColor : sourceTextNode->TextColor.ToVector4(),
+                        TextOutlineColor = sourceTextNode->EdgeColor.ToVector4(),
+                    };
+
+                    Service.AddonController.AttachNode(textNode, gauge->OwnerNode);
+                }
+
+                if (!Throttler.Throttle(throttleKey, 100)) return;
+
+                if (autoAttackNodeID != 0 && isHideAutoAttack)
+                {
+                    var autoAttackNode = addon->GetImageNodeById(autoAttackNodeID);
+                    if (autoAttackNode != null && autoAttackNode->IsVisible())
+                        autoAttackNode->ToggleVisibility(false);
+                }
+
+                textNode.IsVisible = isEnabled;
+                if (!isEnabled) return;
+
+                if (getTarget() is { } target)
+                {
+                    var sourceTextNode = addon->GetTextNodeById(textNodeID);
+                    if (sourceTextNode == null) return;
+
+                    var gauge = addon->GetComponentByNodeId(gaugeNodeID);
+                    if (gauge == null) return;
+
+                    textNode.Position         = position;
+                    textNode.Size             = getSizeFunc(gauge->OwnerNode->Width, gauge->OwnerNode->Height);
+                    textNode.AlignmentType    = alignLeft ? AlignmentType.BottomLeft : AlignmentType.BottomRight;
+                    textNode.FontSize         = fontSize;
+                    textNode.TextColor        = customColor.W != 0 ? customColor : sourceTextNode->TextColor.ToVector4();
+                    textNode.TextOutlineColor = sourceTextNode->EdgeColor.ToVector4();
+
+                    textNode.Text = string.Format(ModuleConfig.DisplayFormatString,
+                                                  FormatNumber(target.MaxHp),
+                                                  FormatNumber(target.CurrentHp));
+                }
+
+                break;
+            case AddonEvent.PreFinalize:
+                Service.AddonController.DetachNode(textNode);
+                textNode = null;
+                break;
+        }
+    }
+
+    private static string FormatNumber(uint num, DisplayFormat? displayFormat = null)
+    {
+        displayFormat ??= ModuleConfig.DisplayFormat;
+        
+        var currentLang = Lang.CurrentLanguage;
+        switch (displayFormat)
+        {
+            case DisplayFormat.FullNumber:
+                return num.ToString();
+            case DisplayFormat.FullNumberSeparators:
+                return num.ToString("N0");
+            case DisplayFormat.ChineseFull:
+                return FormatUtf8NumberByChineseNotation((int)num, currentLang)->ToString();
+            case DisplayFormat.ChineseZeroPrecision:
+            case DisplayFormat.ChineseOnePrecision:
+            case DisplayFormat.ChineseTwoPrecision:
+                var (divisor, unit) = num switch
+                {
+                    >= 1_0000_0000 => (1_0000_0000f, currentLang is ("ChineseTraditional" or "Japanese") ? "億" : "亿"),
+                    >= 1_0000      => (1_0000f, currentLang == "ChineseTraditional" ? "萬" : "万"),
+                    _              => (1f, string.Empty)
+                };
+
+                var value = num / divisor;
+                var fStrChinese = displayFormat switch
+                {
+                    DisplayFormat.ChineseOnePrecision  => "F1",
+                    DisplayFormat.ChineseTwoPrecision  => "F2",
+                    DisplayFormat.ChineseZeroPrecision => "F0"
+                };
+
+                var formattedValue = value.ToString(fStrChinese);
+                formattedValue = formattedValue.TrimEnd('0').TrimEnd('.');
+
+                return $"{formattedValue}{unit}";
+            case DisplayFormat.ZeroPrecision:
+            case DisplayFormat.OnePrecision:
+            case DisplayFormat.TwoPrecision:
+                var fStrEnglish = displayFormat switch {
+                    DisplayFormat.OnePrecision  => "F1",
+                    DisplayFormat.TwoPrecision  => "F2",
+                    DisplayFormat.ZeroPrecision => "F0"
+                };
+
+                return num switch {
+                    >= 1000000 => $"{(num / 1000000f).ToString(fStrEnglish)}M",
+                    >= 1000    => $"{(num / 1000f).ToString(fStrEnglish)}K",
+                    _          => $"{num}"
+                };
+            default:
+                return num.ToString("N0");
+        }
+    }
+
+    protected override void Uninit()
+    {
+        DService.AddonLifecycle.UnregisterListener(OnAddonTargetInfo);
+        OnAddonTargetInfo(AddonEvent.PreFinalize, null);
+        
+        DService.AddonLifecycle.UnregisterListener(OnAddonTargetInfoSplitTarget);
+        OnAddonTargetInfoSplitTarget(AddonEvent.PreFinalize, null);
+        
+        DService.AddonLifecycle.UnregisterListener(OnAddonFocusTargetInfo);
+        OnAddonFocusTargetInfo(AddonEvent.PreFinalize, null);
+    }
+
+    private class Config : ModuleConfiguration
+    {
+        public DisplayFormat DisplayFormat       = DisplayFormat.ChineseOnePrecision;
+        public string        DisplayFormatString = "{0} / {1}";
+
+        public bool    IsEnabled   = true;
+        public Vector2 Position    = new(0);
+        public Vector4 CustomColor = new(1, 1, 1, 0);
+        public byte    FontSize    = 14;
+        public bool    AlignLeft;
+        public bool    HideAutoAttack = true;
+        
+        public bool    FocusIsEnabled   = true;
+        public Vector2 FocusPosition    = new(0);
+        public Vector4 FocusCustomColor = new(1, 1, 1, 0);
+        public byte    FocusFontSize    = 14;
+        public bool    FocusAlignLeft;
+    }
+    
+    public enum DisplayFormat
+    {
+        FullNumber,
+        FullNumberSeparators,
+        ChineseFull,
+        ChineseZeroPrecision,
+        ChineseOnePrecision,
+        ChineseTwoPrecision,
+        ZeroPrecision,
+        OnePrecision,
+        TwoPrecision
+    }
+
+    private static readonly Dictionary<DisplayFormat, string> DisplayFormatLoc = new()
+    {
+        [DisplayFormat.FullNumber]           = GetLoc("OptimizedTargetInfo-FullNumber"),
+        [DisplayFormat.FullNumberSeparators] = GetLoc("OptimizedTargetInfo-FullNumberSeparators"),
+        [DisplayFormat.ChineseFull]          = GetLoc("OptimizedTargetInfo-ChineseFull"),
+        [DisplayFormat.ChineseZeroPrecision] = GetLoc("OptimizedTargetInfo-ChineseZeroPrecision"),
+        [DisplayFormat.ChineseOnePrecision]  = GetLoc("OptimizedTargetInfo-ChineseOnePrecision"),
+        [DisplayFormat.ChineseTwoPrecision]  = GetLoc("OptimizedTargetInfo-ChineseTwoPrecision"),
+        [DisplayFormat.ZeroPrecision]        = GetLoc("OptimizedTargetInfo-ZeroPrecision"),
+        [DisplayFormat.OnePrecision]         = GetLoc("OptimizedTargetInfo-OnePrecision"),
+        [DisplayFormat.TwoPrecision]         = GetLoc("OptimizedTargetInfo-TwoPrecision"),
+    };
+}
