bd825f7bb29d7aad2bded864bf2e9d5b5886c15b
ECSS11 <53446819+Ecss11@users.noreply.github.com>
2025-08-19 00:19:54 -0500

    治疗优化：为重定向复活添加苏生 (#143)
    feat: add action id for 苏生

--- Action/HealerHelper.cs
+++ Action/HealerHelper.cs
@@ -894,7 +894,7 @@ public class HealerHelper : DailyModuleBase
     private class EasyHealManager(EasyHealManager.Storage config)
     {
         // const
-        public static readonly HashSet<uint> RaiseActions = [125, 173, 3603, 24287, 7670, 7523];
+        public static readonly HashSet<uint> RaiseActions = [125, 173, 3603, 24287, 7670, 7523, 64556];
 
         // heal action status
         public bool TargetHealActionsLoaded => config.TargetHealActions.Count > 0;
