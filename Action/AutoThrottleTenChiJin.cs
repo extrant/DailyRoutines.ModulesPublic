07529fb86e577e36524f59ee5a967a63aeaae5e5
AtmoOmen <atmoomen2004@outlook.com>
2025-08-23 18:19:42 +0800

    变更

--- Action/AutoThrottleTenChiJin.cs
+++ Action/AutoThrottleTenChiJin.cs
@@ -23,7 +23,7 @@ public unsafe class AutoThrottleTenChiJin : DailyModuleBase
     
     private static readonly HashSet<uint> UsedShinobiActions = [];
 
-    protected override void Init() => GamePacketManager.Register(OnPreSendActionPacket);
+    protected override void Init() => GamePacketManager.RegPreSendPacket(OnPreSendActionPacket);
     
     private static void OnPreSendActionPacket(ref bool isPrevented, int opcode, ref byte* packet, ref ushort priority)
     {
@@ -47,5 +47,5 @@ public unsafe class AutoThrottleTenChiJin : DailyModuleBase
             UsedShinobiActions.Clear();
     }
 
-    protected override void Uninit() => GamePacketManager.Unregister(OnPreSendActionPacket);
+    protected override void Uninit() => GamePacketManager.Unreg(OnPreSendActionPacket);
 }
