9823bbc6005832af2d9498251a79e3a6f8997ad4
AtmoOmen <atmoomen2004@outlook.com>
2025-09-01 23:09:50 +0800

    AutoQuestAccept

--- UIOperation/AutoQuestAccept.cs
+++ UIOperation/AutoQuestAccept.cs
@@ -3,23 +3,22 @@ using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 
-namespace DailyRoutines.Modules;
+namespace DailyRoutines.ModulesPublic;
 
 public class AutoQuestAccept : DailyModuleBase
 {
     public override ModuleInfo Info { get; } = new()
     {
-        Title = GetLoc("AutoQuestAcceptTitle"),
+        Title       = GetLoc("AutoQuestAcceptTitle"),
         Description = GetLoc("AutoQuestAcceptDescription"),
-        Category = ModuleCategories.UIOperation,
+        Category    = ModuleCategories.UIOperation,
     };
 
-    protected override void Init()
-    {
+    protected override void Init() => 
         DService.AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "JournalAccept", OnAddonSetup);
-    }
 
-    protected override void ConfigUI() { ConflictKeyText(); }
+    protected override void ConfigUI() => 
+        ConflictKeyText();
 
     private unsafe void OnAddonSetup(AddonEvent type, AddonArgs args)
     {
@@ -30,12 +29,13 @@ public class AutoQuestAccept : DailyModuleBase
 
         var questID = addon->AtkValues[261].UInt;
         if (questID == 0) return;
+
+        var isAcceptable = addon->AtkValues[4].UInt;
+        if (isAcceptable == 0) return;
         
         Callback(addon, true, 3, questID);
     }
 
-    protected override void Uninit()
-    {
+    protected override void Uninit() => 
         DService.AddonLifecycle.UnregisterListener(OnAddonSetup);
-    }
 }
