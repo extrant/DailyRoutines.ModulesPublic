ac12d2db943b5265c0bf902c6e2dd34280d326dd
AtmoOmen <atmoomen2004@outlook.com>
2025-09-28 11:49:03 +0800

    AutoMovePetCenter

--- Combat/AutoMovePetCenter.cs
+++ Combat/AutoMovePetCenter.cs
@@ -1,11 +1,8 @@
 using DailyRoutines.Abstracts;
-using DailyRoutines.Infos;
 using DailyRoutines.Managers;
 using Dalamud.Hooking;
-using FFXIVClientStructs.FFXIV.Client.Game;
-using Lumina.Excel.Sheets;
 
-namespace DailyRoutines.Modules;
+namespace DailyRoutines.ModulesPublic;
 
 public unsafe class AutoMovePetCenter : DailyModuleBase
 {
@@ -18,10 +15,9 @@ public unsafe class AutoMovePetCenter : DailyModuleBase
         ModulesConflict = ["AutoMovePetPosition"]
     };
 
-    private static readonly CompSig ProcessPacketSpawnNPCSig = new(
-        "48 89 5C 24 08 57 48 81 EC 30 04 00 00 48 8B DA 8B F9 E8 ?? ?? ?? ?? 3C 01 75 21 E8 ?? ?? ?? ?? 3C 01 75 18 80 BB 82 00 00 00 02 75 0F 8B 05 ?? ?? ?? ?? 39 43 54 0F 85 ?? ?? ?? ?? 0F B6 53 7E 48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 0F B6 53 7E 48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8D 44 24 28 C7 44 24 20 02 00 00 00");
-    private delegate void ProcessPacketSpawnNPCDelegate(nint a1, byte* packetData);
-    private static Hook<ProcessPacketSpawnNPCDelegate>? ProcessPacketSpawnNPCHook;
+    private static readonly CompSig ProcessPacketSpawnNPCSig = new("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 41 56 48 83 EC ?? 48 8B F1 49 8B E8 48 83 C1");
+    private delegate        void ProcessPacketSpawnNPCDelegate(void* a1, byte* packetData);
+    private static          Hook<ProcessPacketSpawnNPCDelegate>? ProcessPacketSpawnNPCHook;
 
     protected override void Init()
     {
@@ -31,34 +27,32 @@ public unsafe class AutoMovePetCenter : DailyModuleBase
         DService.DutyState.DutyStarted += OnDutyStarted;
     }
 
-    protected override void Uninit()
-    {
+    protected override void Uninit() => 
         DService.DutyState.DutyStarted -= OnDutyStarted;
-        base.Uninit();
-    }
 
-    private void OnDutyStarted(object? sender, ushort e) => MovePetToMapCenter();
+    private static void OnDutyStarted(object? sender, ushort e) => 
+        MovePetToMapCenter();
 
-    private void ProcessPacketSpawnNPCDetour(nint a1, byte* packetData)
+    private static void ProcessPacketSpawnNPCDetour(void* a1, byte* packetData)
     {
         ProcessPacketSpawnNPCHook.Original(a1, packetData);
         
         var npcEntityID = *(uint*)(packetData + 84);
-        if (npcEntityID != (DService.ObjectTable.LocalPlayer?.EntityId ?? 0)) return;
+        if (npcEntityID != LocalPlayerState.EntityID) 
+            return;
 
         MovePetToMapCenter();
     }
 
     private static void MovePetToMapCenter()
     {
-        if (!LuminaGetter.TryGetRow<ContentFinderCondition>
-                (GameMain.Instance()->CurrentContentFinderConditionId, out var content) ||
-            content.ContentType.RowId is not (4 or 5)                                     ||
-            DService.ObjectTable.LocalPlayer is null                                    ||
-            !LuminaGetter.TryGetRow<Map>(DService.ClientState.MapId, out var map))
+        if (GameState.ContentFinderCondition == 0                                  ||
+            GameState.ContentFinderConditionData.ContentType.RowId is not (4 or 5) ||
+            DService.ObjectTable.LocalPlayer is null                               ||
+            GameState.Map == 0)
             return;
         
-        var pos = TextureToWorld(new(1024), map).ToVector3();
+        var pos = TextureToWorld(new(1024), GameState.MapData).ToVector3();
         ExecuteCommandManager.ExecuteCommandComplexLocation(ExecuteCommandComplexFlag.PetAction, pos, 3);
     }
 }
