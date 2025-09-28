b849a7a1e9ec2c684295ccb3f38fc6eb847299c6
AtmoOmen <atmoomen2004@outlook.com>
2025-09-28 17:18:45 +0800

    AutoMovePetCenter

--- Combat/AutoMovePetCenter.cs
+++ Combat/AutoMovePetCenter.cs
@@ -15,8 +15,9 @@ public unsafe class AutoMovePetCenter : DailyModuleBase
         ModulesConflict = ["AutoMovePetPosition"]
     };
 
-    private static readonly CompSig ProcessPacketSpawnNPCSig = new("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 41 56 48 83 EC ?? 48 8B F1 49 8B E8 48 83 C1");
-    private delegate        void ProcessPacketSpawnNPCDelegate(void* a1, byte* packetData);
+    private static readonly CompSig ProcessPacketSpawnNPCSig =
+        new("48 89 5C 24 08 57 48 81 EC 30 04 00 00 48 8B DA 8B F9 E8 ?? ?? ?? ?? 3C 01 75 21 E8 ?? ?? ?? ?? 3C 01 75 18 80 BB 82 00 00 00 02 75 0F 8B 05 ?? ?? ?? ?? 39 43 54 0F 85 ?? ?? ?? ?? 0F B6 53 7E 48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 0F B6 53 7E 48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8D 44 24 28 C7 44 24 20 02 00 00 00");
+    private delegate        void ProcessPacketSpawnNPCDelegate(uint targetID, byte* packetData);
     private static          Hook<ProcessPacketSpawnNPCDelegate>? ProcessPacketSpawnNPCHook;
 
     protected override void Init()
@@ -31,25 +32,25 @@ public unsafe class AutoMovePetCenter : DailyModuleBase
         DService.DutyState.DutyStarted -= OnDutyStarted;
 
     private static void OnDutyStarted(object? sender, ushort e) => 
-        MovePetToMapCenter();
+        MovePetToMapCenter(LocalPlayerState.EntityID);
 
-    private static void ProcessPacketSpawnNPCDetour(void* a1, byte* packetData)
+    private static void ProcessPacketSpawnNPCDetour(uint targetID, byte* packetData)
     {
-        ProcessPacketSpawnNPCHook.Original(a1, packetData);
+        ProcessPacketSpawnNPCHook.Original(targetID, packetData);
         
-        var npcEntityID = *(uint*)(packetData + 84);
-        if (npcEntityID != LocalPlayerState.EntityID) 
-            return;
-
-        MovePetToMapCenter();
+        var entityIDPtr = (uint*)(packetData + 84);
+        if (entityIDPtr == null) return;
+        
+        MovePetToMapCenter(*entityIDPtr);
     }
 
-    private static void MovePetToMapCenter()
+    private static void MovePetToMapCenter(uint npcEntityID)
     {
         if (GameState.ContentFinderCondition == 0                                  ||
+            GameState.Map                    == 0                                  ||
+            npcEntityID                      != LocalPlayerState.EntityID          ||
             GameState.ContentFinderConditionData.ContentType.RowId is not (4 or 5) ||
-            DService.ObjectTable.LocalPlayer is null                               ||
-            GameState.Map == 0)
+            DService.ObjectTable.LocalPlayer is null)
             return;
         
         var pos = TextureToWorld(new(1024), GameState.MapData).ToVector3();
