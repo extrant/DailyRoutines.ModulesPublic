7e237737430b7ccc32fce5d67f477004b544f2dd
AtmoOmen <atmoomen2004@outlook.com>
2025-10-04 23:27:13 +0800

    AutoTalkSkip

--- System/AutoTalkSkip.cs
+++ System/AutoTalkSkip.cs
@@ -19,64 +19,66 @@ public unsafe class AutoTalkSkip : DailyModuleBase
 
     private delegate nint TalkDelegate(EventSceneModuleImplBase* scene);
     
-    // Base0: 48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 48 83 EC ?? 48 8B F9 48 8B EA 48 8B 49 ?? E8
-    // Base1: 40 55 56 57 41 55 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 44 24 ??
-    private static readonly CompSig TalkSig =
-        new("40 53 48 83 EC 20 48 8B D9 E8 ?? ?? ?? ?? 48 8B D3 48 8B 88 ?? ?? ?? ?? 48 8B 01 48 83 C4 20 5B 48 FF A0 A8 00 00 00");
-    private static   Hook<TalkDelegate>? TalkHook;
 
-    private static readonly CompSig TalkAsyncSig =
-        new("40 53 48 83 EC 20 48 8B D9 E8 ?? ?? ?? ?? 48 8B D3 48 8B 88 ?? ?? ?? ?? 48 8B 01 48 83 C4 20 5B 48 FF A0 B0 00 00 00");
+    private static readonly CompSig TalkBaseSig0 = new("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 48 83 EC ?? 48 8B F9 48 8B EA 48 8B 49 ?? E8");
+
+    private static Hook<TalkDelegate>? TalkHook;
     private static Hook<TalkDelegate>? TalkAsyncHook;
 
-    private static readonly CompSig SystemTalkSig =
-        new("40 53 48 83 EC 20 48 8B D9 E8 ?? ?? ?? ?? 48 8B D3 48 8B 88 ?? ?? ?? ?? 48 8B 01 48 83 C4 20 5B 48 FF A0 B8 00 00 00");
-    private static Hook<TalkDelegate>? SystemTalkHook;
+    private static readonly CompSig TalkBaseSig1 = new("40 55 56 57 41 55 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 44 24");
+
+    private static Hook<TalkDelegate>?        SystemTalkHook;
+    private static Hook<LuaFunctionDelegate>? LogMessageNoSkipHook;
     
-    // Base: 48 89 54 24 ?? 53 56 57 41 54 41 55 48 83 EC ?? 48 8B D9
-    private static readonly CompSig ShortTalkSig =
-        new("40 53 48 83 EC 20 48 8B D9 E8 ?? ?? ?? ?? 48 8B D3 48 8B 88 ?? ?? ?? ?? 48 8B 01 48 83 C4 20 5B 48 FF A0 90 03 00 00");
+    private static readonly CompSig TalkBaseSig2 = new("48 89 54 24 ?? 53 56 57 41 54 41 55 48 83 EC ?? 48 8B D9");
+    
     private static Hook<TalkDelegate>? ShortTalkHook;
-    
-    private static readonly CompSig ShortTalkWithLineVoiceSig =
-        new("40 53 48 83 EC 20 48 8B D9 E8 ?? ?? ?? ?? 48 8B D3 48 8B 88 ?? ?? ?? ?? 48 8B 01 48 83 C4 20 5B 48 FF A0 98 03 00 00");
     private static Hook<TalkDelegate>? ShortTalkWithLineVoiceHook;
     
-    private static readonly CompSig CraftLeveTalkSig = new("40 53 48 83 EC 50 48 8B D1 48 8D 4C 24 20 E8 ?? ?? ?? ?? BA 01 00 00 00 48 8D 4C 24 20 E8 ?? ?? ?? ?? 48 8B 4C 24 28 4C 8B C0 BA 01 00 00 00 E8 ?? ?? ?? ?? 48 8B 4C 24 28 BA 02 00 00 00 48 8B 18 E8 ?? ?? ?? ?? 48 85 DB 74 2A 8B D0 48 8B CB E8 ?? ?? ?? ?? 33 D2 48 8D 4C 24 20 E8 ?? ?? ?? ?? 48 8D 4C 24 20 8B D8 E8 ?? ?? ?? ?? 8B C3 48 83 C4 50 5B C3 48 8D 4C 24 20 E8 ?? ?? ?? ?? 8B C3 48 83 C4 50 5B C3 CC CC CC CC CC CC CC CC CC 40 53 48 81 EC 50 01 00 00");
+    private static readonly CompSig TalkBaseSig3 = new("48 89 54 24 ?? 53 57 41 54 41 55 41 56 48 83 EC ?? 48 8B D9 48 8B FA 48 8B 49 ?? E8 ?? ?? ?? ?? 4C 8B 25 ?? ?? ?? ?? 44 8B F0 48 8B 4B ?? 49 8B D4 4C 8B 2D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8B 4B ?? BA ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8B 4B ?? BA ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8B 4B ?? BA ?? ?? ?? ?? 85 C0 74 ?? E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 48 89 6C 24 ?? 4C 89 7C 24 ?? E8 ?? ?? ?? ?? 48 8B 4B ?? 49 8B D4 E8 ?? ?? ?? ?? 48 8B 4B ?? 45 33 C0 33 D2 E8 ?? ?? ?? ?? 41 83 C6 ?? 4C 8D 05 ?? ?? ?? ?? 41 8B D6 4D 8B CC 48 8B CB E8 ?? ?? ?? ?? 41 B9 ?? ?? ?? ?? 4C 8D 05 ?? ?? ?? ?? 41 8B D6 48 8B CB E8 ?? ?? ?? ?? 4C 8D 0D ?? ?? ?? ?? 41 8B D6 4C 8D 05 ?? ?? ?? ?? 48 8B CB E8 ?? ?? ?? ?? 4C 8D 0D ?? ?? ?? ?? 41 8B D6 4C 8D 05 ?? ?? ?? ?? 48 8B CB E8 ?? ?? ?? ?? 4C 8D 0D ?? ?? ?? ?? 41 8B D6 4C 8D 05 ?? ?? ?? ?? 48 8B CB E8 ?? ?? ?? ?? 4C 8D 0D ?? ?? ?? ?? 41 8B D6 4C 8D 05 ?? ?? ?? ?? 48 8B CB E8 ?? ?? ?? ?? 4C 8D 0D ?? ?? ?? ?? 41 8B D6 4C 8D 05 ?? ?? ?? ?? 48 8B CB E8 ?? ?? ?? ?? 4C 8D 0D ?? ?? ?? ?? 41 8B D6 4C 8D 05 ?? ?? ?? ?? 48 8B CB E8 ?? ?? ?? ?? 4C 8D 0D ?? ?? ?? ?? 41 8B D6 4C 8D 05 ?? ?? ?? ?? 48 8B CB E8 ?? ?? ?? ?? E8");
+
     private static Hook<LuaFunctionDelegate>? CraftLeveTalkHook;
     
-    private static readonly CompSig GuildleveAssignmentTalkSig = new("40 53 48 83 EC ?? 48 8B D1 48 8D 4C 24 ?? E8 ?? ?? ?? ?? BA ?? ?? ?? ?? 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8B 4C 24 ?? 4C 8B C0 BA ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8B 4C 24 ?? BA ?? ?? ?? ?? 48 8B 18 E8 ?? ?? ?? ?? 48 85 DB 74 ?? 8B D0 48 8B CB E8 ?? ?? ?? ?? 33 D2 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8D 4C 24 ?? 8B D8 E8 ?? ?? ?? ?? 8B C3 48 83 C4 ?? 5B C3 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 8B C3 48 83 C4 ?? 5B C3 CC CC CC CC CC CC CC CC CC 40 53 56");
+    private static readonly CompSig TalkBaseSig4 = new("E8 ?? ?? ?? ?? 48 8B 4F 08 48 8D 57 10 01 47 3C E8 ?? ?? ?? ?? 48 8B 4F 08 48 8D 57 10 01 47 3C E8 ?? ?? ?? ?? 48 8B 4F 08 48 8D 57 10 01 47 3C E8 ?? ?? ?? ?? 48 8B 4F 08 48 8D 57 10 01 47 3C E8 ?? ?? ?? ?? 48 8B 4F 08 48 8D 57 10 01 47 3C E8 ?? ?? ?? ?? 48 8B 4F 08 48 8D 57 10 01 47 3C E8 ?? ?? ?? ?? 48 8B 4F 08 48 8D 57 10 01 47 3C E8 ?? ?? ?? ?? 48 8B 4F 08 48 8D 57 10 01 47 3C E8 ?? ?? ?? ?? 48 8B 4F 08 48 8D 57 10 01 47 3C E8 ?? ?? ?? ?? FF C0 ");
+    
     private static Hook<LuaFunctionDelegate>? GuildleveAssignmentTalkHook;
-
-    private static readonly CompSig LogMessageNoSkipSig =
-        new("4C 8B DC 55 53 41 57 49 8D 6B ?? 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 45 ?? 48 8B D1");
-    private static Hook<LuaFunctionDelegate>? LogMessageNoSkipHook;
-
+    
     protected override void Init()
     {
-        TalkHook ??= TalkSig.GetHook<TalkDelegate>(TalkDetour);
+        var baseAddress0 = TalkBaseSig0.ScanText();
+        
+        TalkHook ??= DService.Hook.HookFromAddress<TalkDelegate>(GetLuaFunctionByName(baseAddress0, "Talk"), TalkDetour);
         TalkHook.Enable();
 
-        TalkAsyncHook ??= TalkAsyncSig.GetHook<TalkDelegate>(TalkDetour);
+        TalkAsyncHook ??= DService.Hook.HookFromAddress<TalkDelegate>(GetLuaFunctionByName(baseAddress0, "TalkAsync"), TalkDetour);
         TalkAsyncHook.Enable();
         
-        SystemTalkHook ??= SystemTalkSig.GetHook<TalkDelegate>(TalkDetour);
+        var baseAddress1 = TalkBaseSig1.ScanText();
+        
+        SystemTalkHook ??= DService.Hook.HookFromAddress<TalkDelegate>(GetLuaFunctionByName(baseAddress1, "SystemTalk"), TalkDetour);
         SystemTalkHook.Enable();
         
-        ShortTalkHook ??= ShortTalkSig.GetHook<TalkDelegate>(TalkDetour);
+        LogMessageNoSkipHook ??= DService.Hook.HookFromAddress<LuaFunctionDelegate>(GetLuaFunctionByName(baseAddress1, "LogMessageNoSkip"), LuaStateTalkDetour);
+        LogMessageNoSkipHook.Enable();
+        
+        var baseAddress2 = TalkBaseSig2.ScanText();
+        
+        ShortTalkHook ??= DService.Hook.HookFromAddress<TalkDelegate>(GetLuaFunctionByName(baseAddress2, "ShortTalk"), TalkDetour);
         ShortTalkHook.Enable();
         
-        ShortTalkWithLineVoiceHook ??= ShortTalkWithLineVoiceSig.GetHook<TalkDelegate>(TalkDetour);
+        ShortTalkWithLineVoiceHook ??= DService.Hook.HookFromAddress<TalkDelegate>(GetLuaFunctionByName(baseAddress2, "ShortTalkWithLineVoice"), TalkDetour);
         ShortTalkWithLineVoiceHook.Enable();
         
-        CraftLeveTalkHook ??= CraftLeveTalkSig.GetHook<LuaFunctionDelegate>(LuaStateTalkDetour);
+        var baseAddress3 = TalkBaseSig3.ScanText();
+        
+        CraftLeveTalkHook ??= DService.Hook.HookFromAddress<LuaFunctionDelegate>(GetLuaFunctionByName(baseAddress3, "CraftLeveTalk"), LuaStateTalkDetour);
         CraftLeveTalkHook.Enable();
         
-        GuildleveAssignmentTalkHook ??= GuildleveAssignmentTalkSig.GetHook<LuaFunctionDelegate>(LuaStateTalkDetour);
+        var baseAddress4 = TalkBaseSig4.ScanText();
+
+        GuildleveAssignmentTalkHook ??=
+            DService.Hook.HookFromAddress<LuaFunctionDelegate>(GetLuaFunctionByName(baseAddress4, "GuildleveAssignmentTalk"), LuaStateTalkDetour);
         GuildleveAssignmentTalkHook.Enable();
-        
-        LogMessageNoSkipHook ??= LogMessageNoSkipSig.GetHook<LuaFunctionDelegate>(LuaStateTalkDetour);
-        LogMessageNoSkipHook.Enable();
         
         DService.AddonLifecycle.RegisterListener(AddonEvent.PreDraw, "Talk", OnAddon);
     }
