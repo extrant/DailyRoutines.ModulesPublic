1117b7a737c7fc5703afaf784667bd22e8f4c770
AtmoOmen <atmoomen2004@outlook.com>
2025-10-06 17:26:24 +0800

    TheCuffOfTheFatherHelper

---
+++ Assist/TheCuffOfTheFatherHelper.cs
@@ -0,0 +1,54 @@
+﻿using DailyRoutines.Abstracts;
+using DailyRoutines.Managers;
+using Dalamud.Game.ClientState.Conditions;
+using Dalamud.Plugin.Services;
+using FFXIVClientStructs.FFXIV.Client.Game.Object;
+using ObjectKind = Dalamud.Game.ClientState.Objects.Enums.ObjectKind;
+
+namespace DailyRoutines.ModulesPublic;
+
+public class TheCuffOfTheFatherHelper : DailyModuleBase
+{
+    public override ModuleInfo Info { get; } = new()
+    {
+        Title       = GetLoc("TheCuffOfTheFatherHelperTitle"),
+        Description = GetLoc("TheCuffOfTheFatherHelperDescription"),
+        Category    = ModuleCategories.Assist
+    };
+    
+    protected override void Init()
+    {
+        DService.ClientState.TerritoryChanged += OnZoneChanged;
+        OnZoneChanged(0);
+    }
+
+    protected override void Uninit()
+    {
+        DService.ClientState.TerritoryChanged -= OnZoneChanged;
+        FrameworkManager.Unreg(OnUpdate);
+    }
+
+    private static void OnZoneChanged(ushort zone)
+    {
+        FrameworkManager.Unreg(OnUpdate);
+        
+        if (GameState.TerritoryType != 443) return;
+
+        FrameworkManager.Reg(OnUpdate, throttleMS: 500);
+    }
+
+    private static unsafe void OnUpdate(IFramework _)
+    {
+        foreach (var obj in DService.ObjectTable)
+        {
+            if (obj.ObjectKind != ObjectKind.BattleNpc || obj.DataId != 3865) continue;
+            
+            if (DService.Condition[ConditionFlag.Mounted])
+                obj.ToStruct()->TargetableStatus |= ObjectTargetableFlags.IsTargetable;
+            else
+                obj.ToStruct()->TargetableStatus &= ~ObjectTargetableFlags.IsTargetable;
+            
+            obj.ToStruct()->Highlight(ObjectHighlightColor.Yellow);
+        }
+    }
+}
