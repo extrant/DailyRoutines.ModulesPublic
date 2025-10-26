1f733bd8bbb63664c3a606ee3fe346778f05ea8f
AtmoOmen <atmoomen2004@outlook.com>
2025-10-26 19:45:28 +0800

    修改大小

--- UIOptimization/LargerColorantColoringPreviewComponent.cs
+++ UIOptimization/LargerColorantColoringPreviewComponent.cs
@@ -29,67 +29,74 @@ public unsafe class LargerColorantColoringPreviewComponent : DailyModuleBase
         var addon = ColorantColoring;
         if (addon == null) return;
         
+        addon->WindowNode->SetWidth(754);
         for (var i = 0; i < addon->WindowNode->Component->UldManager.NodeListCount; i++)
         {
             var node = addon->WindowNode->Component->UldManager.NodeList[i];
             if (node == null) continue;
 
             if (node->Width == 654)
-                node->SetWidth(674);
+                node->SetWidth(754);
                         
             if (node->Width == 640)
-                node->SetWidth(660);
+                node->SetWidth(740);
             
             if (node->Width == 649)
-                node->SetWidth(669);
+                node->SetWidth(749);
             
             if (node->Width == 646)
-                node->SetWidth(666);
+                node->SetWidth(746);
             
             if (node->X == 621)
-                node->SetXFloat(641);
+                node->SetXFloat(721);
         }
+
+        var previewContainerNode = addon->GetNodeById(70);
+        if (previewContainerNode != null)
+            previewContainerNode->SetYFloat(56);
 
         var previewComponent = addon->GetComponentNodeById(71);
         if (previewComponent != null)
         {
-            previewComponent->SetWidth(252);
-            previewComponent->SetHeight(420);
-            previewComponent->SetXFloat(-10);
+            previewComponent->SetWidth(330);
+            previewComponent->SetHeight(550);
+            
+            previewComponent->SetXFloat(-6);
             previewComponent->SetYFloat(1);
 
             var borderNode = previewComponent->Component->UldManager.SearchNodeById(3);
             if (borderNode != null)
             {
-                borderNode->SetWidth(252);
-                borderNode->SetHeight(420);
+                borderNode->SetWidth(330);
+                borderNode->SetHeight(550);
             }
             
             var collisionNode = previewComponent->Component->UldManager.SearchNodeById(5);
             if (collisionNode != null)
             {
-                collisionNode->SetWidth(252);
-                collisionNode->SetHeight(420);
+                collisionNode->SetWidth(330);
+                collisionNode->SetHeight(550);
             }
             
             var imageNode = previewComponent->Component->UldManager.SearchNodeById(4);
             if (imageNode != null)
             {
-                imageNode->SetWidth(244);
-                imageNode->SetHeight(412);
+                imageNode->SetWidth(322);
+                imageNode->SetHeight(542);
             }
         }
 
         var gearContainerNode = addon->GetNodeById(17);
         if (gearContainerNode != null)
-            gearContainerNode->SetWidth(646);
+            gearContainerNode->SetWidth(386);
 
         for (var i = 72U; i < 81; i++)
         {
             var checkBoxNode = addon->GetComponentNodeById(i);
             if (checkBoxNode == null) continue;
             
-            checkBoxNode->SetYFloat(424);
+            checkBoxNode->SetXFloat(24 + (28 * (i - 72)));
+            checkBoxNode->SetYFloat(556);
         }
     }
 }
