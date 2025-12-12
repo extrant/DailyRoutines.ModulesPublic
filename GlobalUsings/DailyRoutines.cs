2829ec08c5bc3c6aed48ee09069893214686b326
AtmoOmen <atmoomen2004@outlook.com>
2025-12-12 17:28:12 +0800

    DRConfig

--- GlobalUsings/DailyRoutines.cs
+++ GlobalUsings/DailyRoutines.cs
@@ -1,6 +1,5 @@
 ﻿global using static DailyRoutines.Infos.Widgets;
 global using static DailyRoutines.Infos.Extensions;
-global using static DailyRoutines.Managers.DRConfig;
 global using static DailyRoutines.Managers.LanguageManagerExtensions;
 global using static DailyRoutines.Helpers.NotifyHelper;
 global using Lang = DailyRoutines.Managers.LanguageManager;
